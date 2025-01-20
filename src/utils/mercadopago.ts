6516345fb2b6484034627da9096838373ef42c95
JuanPaladea <juanpaladea5@gmail.com>
2025-01-20 15:27:42 -0300

    chore: Update .gitignore and package.json

--- src/utils/mercadopago.ts
+++
@@ -1,4 +0,0 @@
-import { MercadoPagoConfig, Payment } from 'mercadopago';
-import { MERCADOPAGO_ACCESS_TOKEN } from '../config/envs';
-
-export const client = new MercadoPagoConfig({ accessToken: MERCADOPAGO_ACCESS_TOKEN, options: { timeout: 5000, idempotencyKey: 'abc' } });
